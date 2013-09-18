45211007b08717c23b1e395ed5d2634ed4b223fc
amnik <amnikfung@gmail.com>
2013-09-19 00:04:14 +0800

    first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 57 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#57 SMP PREEMPT Wed Sep 18 21:11:15 HKT 2013"
+#define LINUX_COMPILE_BY "Mars"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
